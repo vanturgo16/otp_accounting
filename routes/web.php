3a7ee9b282554879cc5697c32a8ada8832be0865
Imam Haris Syafaat <38088979+ImamHaris@users.noreply.github.com>
2024-01-21 10:00:21 +0700

    Install Template & Create Master Accounting
    - Template
    - Mst Account Type
    - Mst Account Code

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MstAccountCodesController;
+use App\Http\Controllers\MstAccountTypesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+//Route Login
+Route::get('/', [AuthController::class, 'login'])->name('login');
+Route::post('auth/login', [AuthController::class, 'postlogin'])->name('postlogin')->middleware("throttle:5,2");
 
-Route::get('/', function () {
-    return view('welcome');
+//Route Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    //Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    //AccountType
+    Route::get('/accounttype', [MstAccountTypesController::class, 'index'])->name('accounttype.index');
+    Route::post('/accounttype', [MstAccountTypesController::class, 'index'])->name('accounttype.index');
+    Route::post('accounttype/create', [MstAccountTypesController::class, 'store'])->name('accounttype.store');
+    Route::post('accounttype/update/{id}', [MstAccountTypesController::class, 'update'])->name('accounttype.update');
+    Route::post('accounttype/activate/{id}', [MstAccountTypesController::class, 'activate'])->name('accounttype.activate');
+    Route::post('accounttype/deactivate/{id}', [MstAccountTypesController::class, 'deactivate'])->name('accounttype.deactivate');
+    
+    //AccountCode
+    Route::get('/accountcode', [MstAccountCodesController::class, 'index'])->name('accountcode.index');
+    Route::post('/accountcode', [MstAccountCodesController::class, 'index'])->name('accountcode.index');
+    Route::post('accountcode/create', [MstAccountCodesController::class, 'store'])->name('accountcode.store');
+    Route::post('accountcode/update/{id}', [MstAccountCodesController::class, 'update'])->name('accountcode.update');
+    Route::post('accountcode/activate/{id}', [MstAccountCodesController::class, 'activate'])->name('accountcode.activate');
+    Route::post('accountcode/deactivate/{id}', [MstAccountCodesController::class, 'deactivate'])->name('accountcode.deactivate');
+
 });
